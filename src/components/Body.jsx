004f040fb3229fdcf6c3d38f65000bff3ec08e0c
Rakesh-ganiger <ganigerrakesh1709@gmail.com>
2024-12-27 22:32:57 +0530

    set up the redux store

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,12 +3,15 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 
 
+
+
 const Body = () => {
   return (
     <div>
         <Navbar/>
         <Outlet/>
         <Footer/>
+        
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,27 @@
 import axios from "axios";
 import { useState } from "react"
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants.JS";
+
 
 const Login = () => {
     const[email, setEmail] = useState("dhoni123@gmail.com");
-    const[password, setPassword] = useState("Dhoni@2002")
+    const[password, setPassword] = useState("Dhoni@2002");
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
 
     const handleLogin = async ()=>{
         try {
-            const res=await axios.post("http://localhost:7777/login",{
+            const res=await axios.post( BASE_URL + "/login",{
                 email,
                 password
             },{withCredentials:true})
-            // console.log(res.data)
+            
+            dispatch(addUser(res.data));
+            return navigate("/")
+
         } catch (error) {
             console.error(error)
         }
